96d36209775fd9d24b4626bdeb9f386ef31f0154
Monisha <djmonisha.2022@gmail.com>
2024-12-20 00:58:50 +0530

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,  
-    host: '0.0.0.0' 
-  }
-})
+    port: 5173,
+    host: '0.0.0.0',
+  },
+  build: {
+    outDir: 'dist',  // Ensure the build output goes to the 'dist' directory
+  },
+});
